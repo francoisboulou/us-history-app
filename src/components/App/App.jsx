f758d35fded83327066b8f2e2935a2cdc5fde79a
francoisboulou <giscardboulou@gmail.com>
2019-08-15 12:57:37 -0600

    useEffect was re-rendering events object on lat or lng change, and resulting in multiple renders of markers, especially bad on ombile where location setting is less consistent. Adding bool switch to state to prevent, now marker locations are only set on initial get of lat and lng,future changes are ignored. lint throws dependency warning on useEffect

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,30 +1,33 @@
 import React, { useState, useEffect } from "react"; //useEffect
 import "./App.css";
 import Map from "./Map";
-import historyAPI from "../Utils/historyAPI";
+import APIResponse from "../Utils/APIResponse";
+import coordArray from "../Utils/coordArray";
 
 export default function App() {
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
   const [events, setEvents] = useState(null);
+  const [latLngRes, setRes] = useState(false);
 
   if ("geolocation" in navigator) {
     navigator.geolocation.getCurrentPosition(position => {
       setLatitude(position.coords.latitude);
       setLongitude(position.coords.longitude);
+      setRes(true);
     });
   }
   // else {  /* geolocation IS NOT available */}
 
-  useEffect(
-    () => {
-      if (latitude && longitude) {
-        setEvents(historyAPI(latitude, longitude));
-      }
-    },
-    // esling-disable-next-line
-    [latitude, longitude]
-  );
+  useEffect(() => {
+    if (latLngRes) {
+      const eventCoords = coordArray(latitude, longitude);
+      APIResponse.forEach((response, index) => {
+        response.location = eventCoords[index];
+      });
+      setEvents(APIResponse);
+    }
+  }, [latLngRes]);
 
   if (latitude === null || longitude === null || events === null) {
     return <div>Loading</div>;
---
+++ src/components/Utils/APIResponse.js
@@ -0,0 +1,209 @@
+import test1 from "../../assets/images/App/Popups/test_01.jpg";
+import test2 from "../../assets/images/App/Popups/test_02.jpg";
+import test3 from "../../assets/images/App/Popups/test_03.jpg";
+import test4 from "../../assets/images/App/Popups/test_04.jpg";
+import test5 from "../../assets/images/App/Popups/test_05.jpg";
+import test6 from "../../assets/images/App/Popups/test_06.jpg";
+import test7 from "../../assets/images/App/Popups/test_07.jpg";
+import test8 from "../../assets/images/App/Popups/test_08.jpg";
+import test9 from "../../assets/images/App/Popups/test_09.jpg";
+import test10 from "../../assets/images/App/Popups/test_10.jpg";
+import test11 from "../../assets/images/App/Popups/test_11.jpg";
+import test12 from "../../assets/images/App/Popups/test_12.jpg";
+import test13 from "../../assets/images/App/Popups/test_13.jpg";
+import test14 from "../../assets/images/App/Popups/test_14.jpg";
+import test15 from "../../assets/images/App/Popups/test_15.jpg";
+import test16 from "../../assets/images/App/Popups/test_16.jpg";
+import test17 from "../../assets/images/App/Popups/test_17.jpg";
+import test18 from "../../assets/images/App/Popups/test_18.jpg";
+import test19 from "../../assets/images/App/Popups/test_19.jpg";
+import test20 from "../../assets/images/App/Popups/test_20.jpg";
+import test21 from "../../assets/images/App/Popups/test_21.jpg";
+import test22 from "../../assets/images/App/Popups/test_22.jpg";
+import test23 from "../../assets/images/App/Popups/test_23.jpg";
+import test24 from "../../assets/images/App/Popups/test_24.jpg";
+import test25 from "../../assets/images/App/Popups/test_25.jpg";
+
+const APIResponse = [
+  {
+    title: "Ancient Egyptian Strike",
+    description: "The very first strike recorded in history",
+    text:
+      "Going on strike, you would presume, is closely linked to the history of industrialisation and the formation of trade unions. Wrong! While it was of course the industrialisation of economies that led to better organised work forces, the idea of putting down tools because of a dispute goes back a very long way indeed.",
+    image: test1
+  },
+  {
+    title: "Fantasy Fight Becomes a Reality",
+    description: "It was a publicity stunt, but it was hugely popular.",
+    text:
+      "The film Rocky Balboa (2006) has a strange premise: after the current heavyweight champion sees a computer-generated fight between himself and Rocky, he gets the Italian Stallion out of retirement for a bout. But this somewhat ridiculous scenario does have a historic precedent.",
+    image: test2
+  },
+  {
+    title: "The dancing plague",
+    description: "a truly bizarre medieval phenomenon from central Europe",
+    text:
+      "It involved spontaneous and continuous dancing by crowds of people until they collapsed through exhaustion – or worse, died. Bizarre as it sounds, choreomania was regularly reported by eyewitnesses and was a genuine concern for authorities. It also seems to have been contagious – for example, in June 1374 one of the widest outbreaks began in Aachen, Germany, before spreading to other places such as Cologne, Flanders, Utrecht, and later Italy.",
+    image: test3
+  },
+  {
+    title: "The American invasion of Korea",
+    description: "Nope, not the one in the 1950s – the one in 1871.",
+    text:
+      "In the 19th century a number of Asian nations sealed themselves off from the outside world – most famously Japan and China, but Korea too. America had decided to ‘unlock’ these Asian states and trade with them. It had worked well in Japan, with the diplomatic mission led by Commodore Perry in the 1850s, but the idea was put on hold during the American Civil War. It wasn’t until 1871 that a small fleet of American ships returned to the Pacific and travelled to the coastline of Korea. The American diplomatic vessel (which was a merchant ship,not a warship) came in towards the shore and was fired at by Korean shore batteries.",
+    image: test4
+  },
+  {
+    title: "An underwhelming WW2 battle",
+    description: "one of the most curious battles of the conflict.",
+    text:
+      "On 6 May 1945, peace was on the horizon and the Third Reich was collapsing. With the German commander (also in charge of Dachau) committing suicide, and some of the Waffen SS soldiers retreating, one of the prisoners, Zvonimir Čučković, a Yugoslav freedom fighter, escaped and went looking for some Allied troops to rescue the rest of the prisoners.",
+    image: test5
+  },
+  {
+    title: "The immovable interest rate",
+    description: "Britain did not invent banking as we know it today",
+    text:
+      "many of the concepts were copied from Holland – but with a growing empire, England rapidly became the master of what today is termed as “modern banking”. The Bank of England was founded in 1694 and was given exclusive possession of the government’s balances. It was additionally given permission to be the only corporation (rather than the government) to issue bank notes. What the bank also did in the same year was set the first national interest rate at six per cent.",
+    image: test6
+  },
+  {
+    title: "The sack of Baltimore",
+    description:
+      "It had little to contribute to history until the summer of 1631, when it was attacked – but by whom?",
+    text:
+      "Was it the French planning an invasion of England via the Emerald Isle? Or perhaps it was some kind of pro-Catholic uprising leading to a vicious English assault, or the Spanish up to their old tricks? You might be surprised to learn that the perpetrators were about as exotic as it gets for the 17th century – Barbary pirates from North Africa led by a Dutch captain (and Muslim convert)-turned pirate, Jan Janszoon van Haarlem, also known as Murad Reis the Younger.",
+    image: test7
+  },
+  {
+    title: "Ancient Egyptian Strike",
+    description: "The very first strike recorded in history",
+    text:
+      "Going on strike, you would presume, is closely linked to the history of industrialisation and the formation of trade unions. Wrong! While it was of course the industrialisation of economies that led to better organised work forces, the idea of putting down tools because of a dispute goes back a very long way indeed.",
+    image: test8
+  },
+  {
+    title: "Fantasy Fight Becomes a Reality",
+    description: "It was a publicity stunt, but it was hugely popular.",
+    text:
+      "The film Rocky Balboa (2006) has a strange premise: after the current heavyweight champion sees a computer-generated fight between himself and Rocky, he gets the Italian Stallion out of retirement for a bout. But this somewhat ridiculous scenario does have a historic precedent.",
+    image: test9
+  },
+  {
+    title: "The dancing plague",
+    description: "a truly bizarre medieval phenomenon from central Europe",
+    text:
+      "It involved spontaneous and continuous dancing by crowds of people until they collapsed through exhaustion – or worse, died. Bizarre as it sounds, choreomania was regularly reported by eyewitnesses and was a genuine concern for authorities. It also seems to have been contagious – for example, in June 1374 one of the widest outbreaks began in Aachen, Germany, before spreading to other places such as Cologne, Flanders, Utrecht, and later Italy.",
+    image: test10
+  },
+  {
+    title: "The American invasion of Korea",
+    description: "Nope, not the one in the 1950s – the one in 1871.",
+    text:
+      "In the 19th century a number of Asian nations sealed themselves off from the outside world – most famously Japan and China, but Korea too. America had decided to ‘unlock’ these Asian states and trade with them. It had worked well in Japan, with the diplomatic mission led by Commodore Perry in the 1850s, but the idea was put on hold during the American Civil War. It wasn’t until 1871 that a small fleet of American ships returned to the Pacific and travelled to the coastline of Korea. The American diplomatic vessel (which was a merchant ship,not a warship) came in towards the shore and was fired at by Korean shore batteries.",
+    image: test11
+  },
+  {
+    title: "An underwhelming WW2 battle",
+    description: "one of the most curious battles of the conflict.",
+    text:
+      "On 6 May 1945, peace was on the horizon and the Third Reich was collapsing. With the German commander (also in charge of Dachau) committing suicide, and some of the Waffen SS soldiers retreating, one of the prisoners, Zvonimir Čučković, a Yugoslav freedom fighter, escaped and went looking for some Allied troops to rescue the rest of the prisoners.",
+    image: test12
+  },
+  {
+    title: "The immovable interest rate",
+    description: "Britain did not invent banking as we know it today",
+    text:
+      "many of the concepts were copied from Holland – but with a growing empire, England rapidly became the master of what today is termed as “modern banking”. The Bank of England was founded in 1694 and was given exclusive possession of the government’s balances. It was additionally given permission to be the only corporation (rather than the government) to issue bank notes. What the bank also did in the same year was set the first national interest rate at six per cent.",
+    image: test13
+  },
+  {
+    title: "The sack of Baltimore",
+    description:
+      "It had little to contribute to history until the summer of 1631, when it was attacked – but by whom?",
+    text:
+      "Was it the French planning an invasion of England via the Emerald Isle? Or perhaps it was some kind of pro-Catholic uprising leading to a vicious English assault, or the Spanish up to their old tricks? You might be surprised to learn that the perpetrators were about as exotic as it gets for the 17th century – Barbary pirates from North Africa led by a Dutch captain (and Muslim convert)-turned pirate, Jan Janszoon van Haarlem, also known as Murad Reis the Younger.",
+    image: test14
+  },
+  {
+    title: "Ancient Egyptian Strike",
+    description: "The very first strike recorded in history",
+    text:
+      "Going on strike, you would presume, is closely linked to the history of industrialisation and the formation of trade unions. Wrong! While it was of course the industrialisation of economies that led to better organised work forces, the idea of putting down tools because of a dispute goes back a very long way indeed.",
+    image: test15
+  },
+  {
+    title: "Fantasy Fight Becomes a Reality",
+    description: "It was a publicity stunt, but it was hugely popular.",
+    text:
+      "The film Rocky Balboa (2006) has a strange premise: after the current heavyweight champion sees a computer-generated fight between himself and Rocky, he gets the Italian Stallion out of retirement for a bout. But this somewhat ridiculous scenario does have a historic precedent.",
+    image: test16
+  },
+  {
+    title: "The dancing plague",
+    description: "a truly bizarre medieval phenomenon from central Europe",
+    text:
+      "It involved spontaneous and continuous dancing by crowds of people until they collapsed through exhaustion – or worse, died. Bizarre as it sounds, choreomania was regularly reported by eyewitnesses and was a genuine concern for authorities. It also seems to have been contagious – for example, in June 1374 one of the widest outbreaks began in Aachen, Germany, before spreading to other places such as Cologne, Flanders, Utrecht, and later Italy.",
+    image: test17
+  },
+  {
+    title: "The American invasion of Korea",
+    description: "Nope, not the one in the 1950s – the one in 1871.",
+    text:
+      "In the 19th century a number of Asian nations sealed themselves off from the outside world – most famously Japan and China, but Korea too. America had decided to ‘unlock’ these Asian states and trade with them. It had worked well in Japan, with the diplomatic mission led by Commodore Perry in the 1850s, but the idea was put on hold during the American Civil War. It wasn’t until 1871 that a small fleet of American ships returned to the Pacific and travelled to the coastline of Korea. The American diplomatic vessel (which was a merchant ship,not a warship) came in towards the shore and was fired at by Korean shore batteries.",
+    image: test18
+  },
+  {
+    title: "An underwhelming WW2 battle",
+    description: "one of the most curious battles of the conflict.",
+    text:
+      "On 6 May 1945, peace was on the horizon and the Third Reich was collapsing. With the German commander (also in charge of Dachau) committing suicide, and some of the Waffen SS soldiers retreating, one of the prisoners, Zvonimir Čučković, a Yugoslav freedom fighter, escaped and went looking for some Allied troops to rescue the rest of the prisoners.",
+    image: test19
+  },
+  {
+    title: "The immovable interest rate",
+    description: "Britain did not invent banking as we know it today",
+    text:
+      "many of the concepts were copied from Holland – but with a growing empire, England rapidly became the master of what today is termed as “modern banking”. The Bank of England was founded in 1694 and was given exclusive possession of the government’s balances. It was additionally given permission to be the only corporation (rather than the government) to issue bank notes. What the bank also did in the same year was set the first national interest rate at six per cent.",
+    image: test20
+  },
+  {
+    title: "The sack of Baltimore",
+    description:
+      "It had little to contribute to history until the summer of 1631, when it was attacked – but by whom?",
+    text:
+      "Was it the French planning an invasion of England via the Emerald Isle? Or perhaps it was some kind of pro-Catholic uprising leading to a vicious English assault, or the Spanish up to their old tricks? You might be surprised to learn that the perpetrators were about as exotic as it gets for the 17th century – Barbary pirates from North Africa led by a Dutch captain (and Muslim convert)-turned pirate, Jan Janszoon van Haarlem, also known as Murad Reis the Younger.",
+    image: test21
+  },
+  {
+    title: "Ancient Egyptian Strike",
+    description: "The very first strike recorded in history",
+    text:
+      "Going on strike, you would presume, is closely linked to the history of industrialisation and the formation of trade unions. Wrong! While it was of course the industrialisation of economies that led to better organised work forces, the idea of putting down tools because of a dispute goes back a very long way indeed.",
+    image: test22
+  },
+  {
+    title: "Fantasy Fight Becomes a Reality",
+    description: "It was a publicity stunt, but it was hugely popular.",
+    text:
+      "The film Rocky Balboa (2006) has a strange premise: after the current heavyweight champion sees a computer-generated fight between himself and Rocky, he gets the Italian Stallion out of retirement for a bout. But this somewhat ridiculous scenario does have a historic precedent.",
+    image: test23
+  },
+  {
+    title: "The dancing plague",
+    description:
+      "choreomania was a truly bizarre medieval phenomenon from central Europe",
+    text:
+      "It involved spontaneous and continuous dancing by crowds of people until they collapsed through exhaustion – or worse, died. Bizarre as it sounds, choreomania was regularly reported by eyewitnesses and was a genuine concern for authorities. It also seems to have been contagious – for example, in June 1374 one of the widest outbreaks began in Aachen, Germany, before spreading to other places such as Cologne, Flanders, Utrecht, and later Italy.",
+    image: test24
+  },
+  {
+    title: "The American invasion of Korea",
+    description: "Nope, not the one in the 1950s – the one in 1871.",
+    text:
+      "In the 19th century a number of Asian nations sealed themselves off from the outside world – most famously Japan and China, but Korea too. America had decided to ‘unlock’ these Asian states and trade with them. It had worked well in Japan, with the diplomatic mission led by Commodore Perry in the 1850s, but the idea was put on hold during the American Civil War. It wasn’t until 1871 that a small fleet of American ships returned to the Pacific and travelled to the coastline of Korea. The American diplomatic vessel (which was a merchant ship,not a warship) came in towards the shore and was fired at by Korean shore batteries.",
+    image: test25
+  }
+];
+
+export default APIResponse;
